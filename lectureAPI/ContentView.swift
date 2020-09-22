43aaf1c7c325d6020ae26a183cfc499eb1e2663a
Philippe MICHEL <philippe.michel1@gmail.com>
2020-09-22 17:23:23 +0200

    Initial Commit

---
+++ lectureAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lectureAPI
+//
+//  Created by Philippe MICHEL on 22/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lectureAPI/lectureAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  lectureAPIApp.swift
+//  lectureAPI
+//
+//  Created by Philippe MICHEL on 22/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct lectureAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
