e62fb583397ba715102bdf21d2ac7b7eabea60df
Philippe MICHEL <philippe.michel1@gmail.com>
2020-09-22 22:37:54 +0200

    Création de la vue Liste réutilisable à la place de la vue contentView La vue contentView apple la vue liste

--- lectureAPI/ContentView.swift
+++ lectureAPI/ContentView.swift
@@ -8,32 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject var connexion = APIRequete()
     
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(connexion.listeUtilisateurs,id: \.id) { donnee  in
-                    HStack {
-                        Image(systemName: "person.circle")
-                            .foregroundColor(.red)
-                        NavigationLink(
-                            destination: DetailUtilisateurs(),
-                            label: {
-                                Text(" nom: \(donnee.name)")
-                            })
-                    }
-                }
-            }
-            .navigationTitle("Liste des Utilisateurs")
-            .onAppear {
-                self.connexion.startRequeteJSONDecoder()
-            }
-        } // fin body
-        
+        MaListe()
     }
     
-} // fin some
+} // fin struct content View
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ lectureAPI/MaListe.swift
@@ -0,0 +1,41 @@
+//
+//  MaListe.swift
+//  lectureAPI
+//
+//  Created by Philippe MICHEL on 22/09/2020.
+//
+
+import SwiftUI
+
+struct MaListe: View {
+    @StateObject var connexion = APIRequete()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(connexion.listeUtilisateurs,id: \.id) { donnee  in
+                    HStack {
+                        Image(systemName: "person.circle")
+                            .foregroundColor(.red)
+                        NavigationLink(
+                            destination: DetailUtilisateurs(),
+                            label: {
+                                Text(" nom: \(donnee.name)")
+                            })
+                    }
+                }
+            }
+            .navigationTitle("Liste des Utilisateurs")
+            .onAppear {
+                self.connexion.startRequeteJSONDecoder()
+            }
+        } // fin body
+        
+    }
+}
+
+struct MaListe_Previews: PreviewProvider {
+    static var previews: some View {
+        MaListe()
+    }
+}
